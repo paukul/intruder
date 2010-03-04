c840efbbec0267b3c1ad8af9218b7045e2d74f6e
Pascal Friederich <paukul@gmail.com>
2010-03-05 00:53:24 +0100

    struct wrapping scares the shit out of me

--- erl_node.c
+++ erl_node.c
@@ -1,44 +1,63 @@
 #include "ruby.h"
 #include "erl_interface.h"
+#include "ei.h"
+#include <stdlib.h>
+#include <string.h>
 
 VALUE ErlNode = Qnil;
-static int node_count = 0;
+int node_count = 0;
 
 /* prototypes */
 void Init_erl_node();
 
-/* ruby methods */
-static VALUE erl_node_init(VALUE self, VALUE url, VALUE cookie);
-static VALUE method_test(VALUE self);
+/* ruby instance methods */
+static VALUE erl_node_init(VALUE self, VALUE host, VALUE sname, VALUE cookie);
+
+/* ruby class methods */
+static VALUE erl_node_new(VALUE class, VALUE host, VALUE sname, VALUE cookie);
 
 /* internal methods */
 static void declare_attr_accessors();
 
 /* implementation */
-static VALUE erl_node_init(VALUE self, VALUE url, VALUE cookie){
-  node_count++;
-  rb_iv_set(self, "@url", url);
+static VALUE erl_node_init(VALUE self, VALUE host, VALUE sname, VALUE cookie){
+  rb_iv_set(self, "@host", host);
+  rb_iv_set(self, "@sname", sname);
   rb_iv_set(self, "@cookie", cookie);
-  rb_iv_set(self, "@nodename", rb_str_new2("c" + node_count));
+
+  /* the erlang cnode struct */
+  ei_cnode *node;
+  /* get the wrapped struct from the ruby object */
+  Data_Get_Struct(self, ei_cnode, node);
+  /* connect the node */
+  ei_connect_init(node, STR2CSTR(host), STR2CSTR(cookie), node_count++);
+
+/*   char buff[5]; */
+/*   sprintf(buff, "%d", ++node_count); */
+/*   char *node_count_str = strcat("c_", buff); */
+/*   b_iv_set(self, "@nodename", rb_str_new2(node_count_str)); */
 }
 
-static VALUE method_test(VALUE self){
-  return rb_str_new2("foobar");
+static VALUE erl_node_new(VALUE class, VALUE host, VALUE sname, VALUE cookie){
+  ei_cnode *node;
+  VALUE ruby_node = Data_Make_Struct(class, ei_cnode, 0, free, node);
 }
 
 void Init_erl_node(){
   ErlNode = rb_define_class("ErlNode", rb_cObject);
   declare_attr_accessors();
 
+  /* class methods */
+  rb_define_singleton_method(ErlNode, "new", erl_node_new, 3);
+
   /* instance methods */
-  rb_define_method(ErlNode, "test", method_test, 0);
-  rb_define_method(ErlNode, "initialize", erl_node_init, 2);
+  rb_define_method(ErlNode, "initialize", erl_node_init, 3);
 }
 
 static void declare_attr_accessors(){
   ID attr_accessor = rb_intern("attr_accessor");
 
-  char  *i_vars[3] = {"url", "cookie", "nodename"};
+  char  *i_vars[3] = {"host", "cookie", "sname"};
   VALUE params[3];
 
   int i = 0;
--- test.rb
+++ test.rb
@@ -1,7 +1,8 @@
 `make clean; make`
 require 'erl_node'
 
-n = ErlNode.new('rabbot@localhost', File.read(File.expand_path('~/.erlang.cookie')))
+n = ErlNode.new('foo', 'localhost', File.read(File.expand_path('~/.erlang.cookie')))
 
 p n
-p n.nodename
+
+#p n.node
