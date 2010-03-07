cff4c5a3bf8116a4ee2798966de30537e3d2c4ff
Pascal Friederich <paukul@gmail.com>
2010-03-07 22:33:20 +0100

    generic test script

--- test.rb
+++ test.rb
@@ -1,10 +1,11 @@
 `make clean; make all`
 require File.expand_path('../intruder', __FILE__)
+hostname = `hostname`.chomp
+
+n = Intruder::Node.new('fooz', File.read(File.expand_path('~/.erlang.cookie')))
 
 puts "rabbit call"
-n = Intruder::Node.new('fooz', File.read(File.expand_path('~/.erlang.cookie')))
-p n
-n.connect('rabbit@codeslave')
+n.connect("rabbit@#{hostname}")
 m = n.mod('rabbit')
 m.status('')
 
